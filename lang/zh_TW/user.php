41f4b0a104475471f5967fb037638b4a6a238b12
Lei123 <s10422027@gmail.com>
2023-04-27 10:56:18 +0800

    初始環境設置

---
+++ lang/zh_TW/user.php
@@ -0,0 +1,16 @@
+<?php 
+return [
+    'labels' => [
+        'User' => 'User',
+        'user' => 'User',
+    ],
+    'fields' => [
+        'name' => '名字',
+        'email' => '電子信箱',
+        'email_verified_at' => '信箱驗證',
+        'password' => '密碼',
+        'remember_token' => '密碼提醒',
+    ],
+    'options' => [
+    ],
+];
